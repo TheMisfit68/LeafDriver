c61226a37736bfc11ea3a5a52530eff4ffc840e5
TheMisfit68 <janverrept@me.com>
2023-09-06 00:58:44 +0200

    Updated swift Package to 5.9

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,9 +14,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "CryptoSwift", url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from:"1.2.0")),
-        .package(name: "SiriDriver", url: "https://github.com/TheMisfit68/SiriDriver.git", .branch("master")),
-        .package(name: "JVCocoa", url: "https://github.com/TheMisfit68/JVCocoa.git",  .branch("master"))
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from:"1.2.0")),
+        .package(url: "https://github.com/TheMisfit68/SiriDriver.git", branch: "master"),
+        .package(url: "https://github.com/TheMisfit68/JVCocoa.git", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
